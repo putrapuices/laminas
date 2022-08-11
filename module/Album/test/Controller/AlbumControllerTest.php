9e46213bc6c5494e130edcb4b7a149983d451557
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-11 14:46:44 +0700

    belajar unittesting phpunit

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AlbumTest\Controller;
+
+use Album\Controller\AlbumController;
+use Laminas\Stdlib\ArrayUtils;
+use Laminas\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase
+{
+    // protected $traceError = false;
+    protected $traceError = true;
+    protected function setUp(): void
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            // Grabbing the full application configuration:
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+        parent::setUp();
+
+        $services = $this->getApplicationServiceLocator();
+        $config = $services->get('config');
+        unset($config['db']);
+        $services->setAllowOverride(true);
+        $services->setService('config', $config);
+        $services->setAllowOverride(false);
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/album');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
+    }
+}
